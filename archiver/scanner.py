f0d29764efbd3727bc008c1186cd8619ed48f7cb
YuviPanda <yuvipanda@gmail.com>
2021-05-18 22:29:47 +0530

    Optimize recursive function a little
    Only recurse on directories, not files. This should
    cut down on a lot of recursion

--- archiver/scanner.py
+++ archiver/scanner.py
@@ -1,27 +1,29 @@
 """
-Scan directories to see if they have files modified since a time
+Scan directories to see if they have files modified in the last x days
 """
 import argparse
 from pathlib import Path
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 def walk(path: Path):
     stat = path.stat()
-    if path.is_file():
-        return stat.st_size, stat.st_mtime
-    else:
-        total_size = stat.st_size
-        newest_modified = stat.st_mtime
-        for c in path.iterdir():
-            # Only accept files, directories and symlinks that point to a valid target
-            if c.is_file() or c.is_dir() or (c.is_symlink() and c.exists()):
-                size, modified = walk(c)
-                total_size += size
-                if modified > newest_modified:
-                    newest_modified = modified
-        return total_size, newest_modified
+    total_size = stat.st_size
+    newest_modified = stat.st_mtime
 
+    for c in path.iterdir():
+        # Only operate on files, non-broken symlinks and directories
+        if c.is_file() or (c.is_symlink() and c.exists()):
+            fstat = c.stat()
+            total_size += fstat.st_size
+            if fstat.st_mtime > newest_modified:
+                newest_modified = fstat.st_mtime
+        elif c.is_dir():
+            size, modified = walk(c)
+            total_size += size
+            if modified > newest_modified:
+                newest_modified = modified
+    return total_size, newest_modified
 
 def main():
     argparser = argparse.ArgumentParser()
