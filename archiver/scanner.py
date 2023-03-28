d07dd1b284d3445816e1fe15660fdeb849627d2d
Jonathan Felder <felder@berkeley.edu>
2023-03-28 13:21:32 -0700

    changing line spacing of template

--- archiver/scanner.py
+++ archiver/scanner.py
@@ -41,12 +41,12 @@ import shutil
 NOTICE_CONTENT_TEMPLATE = """
 Your {hub} files have been archived due to inactivity.
 
-If you want to retrieve a copy of your files through email,
-please open a Data Archival Request via GitHub using the custom URL below:
+If you want to retrieve a copy of your files through email, please open a Data Archival Request via GitHub using the custom URL below:
+
 https://github.com/berkeley-dsep-infra/datahub/issues/new?assignees=&labels=support&template=data_archival_request.yml&title=Request+my+old+files+stored+in+Datahub&link={object_id}
 
-After submitting the form, a Datahub admin will send you a signed link to 
-download your Datahub files via email within a few days.
+After submitting the form, a Datahub admin will send you a signed link to download your Datahub files via email within a few days.
+
 """
 
 
